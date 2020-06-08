9bb05fe0a56eb1e0662c1bbab45fbb0c521f7455
anshulbh9 <49300837+anshulbh9@users.noreply.github.com>
2020-06-08 16:58:40 +0530

    Create powerof2.cpp

---
+++ powerof2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+       if(n<=0){
+           return false;
+       }
+        int x=n&(n-1);
+        if(x==0){
+return true;}
+        return false;
+    }
+};
