626a957f8c74f4fa979c26fddc577cc70659f342
anshulbh9 <49300837+anshulbh9@users.noreply.github.com>
2020-06-14 22:08:32 +0530

    Create  RemoveElement.cpp

---
+++ RemoveElement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int left=0,right=nums.size()-1;
+        while(left<=right){
+            if(nums[left]==val){
+                swap(nums[left],nums[right]);
+                right--;
+            }
+            else{
+            left++;
+            }
+        }
+       return right+1;
+    }
+};
